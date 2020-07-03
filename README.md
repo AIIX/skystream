2be0b49d36d921c366a2d5993cda0f4f7e6ad245
Aditya Mehra <Aix.m@outlook.com>
2020-07-03 13:24:54 +0530

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# skystream
-Play Live News from Sky News with Mycroft
+# <img src='https://raw.githubusercontent.com/FortAwesome/Font-Awesome/master/svgs/regular/newspaper.svg' card_color='#4C2C96' width='50' height='50' style='vertical-align:bottom'/> Sky Stream
+Play Live News from Sky News
+
+## About
+Play Live News from Sky News
+
+## Examples
+* "Hey Mycroft Play Sky Live News"
+* "Hey Mycroft Show Sky Live Stream"
+
+## Credits
+Aix (@aiix)
+
+## Supported Devices 
+platform_mark2 platform_plasmoid 
+
+## Category
+**Media**
+
+## Tags
+#SkyNews
+#Music
+#Video
+#VideoStreaming
+#AudioStreaming
+#LiveStreams
+#Media
+#GUI
---
+++ __init__.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+import time
+from mycroft.skills.core import MycroftSkill, intent_file_handler
+from mycroft.messagebus.message import Message
+from mycroft.util.log import LOG
+
+__author__ = 'aix'
+
+class SkyStream(MycroftSkill):
+    def __init__(self):
+        super(SkyStream, self).__init__(name="SkyStream")
+
+    def initialize(self):
+        LOG.info("Initialized")
+        self.bus.on('skystream.aiix.home', self.launcher_id)
+
+    # Create an intent for playing Sky News Live Stream
+    # Lets use a intent file to be able to call this function
+    
+    def launcher_id(self, message):
+        self.prepare_homepage({})
+        
+    def prepare_homepage(self, message):
+        self.gui.clear()
+        self.enclosure.display_manager.remove_active()
+        self.gui["loadingStatus"] = ""
+        self.gui.show_page("StreamLogo.qml", override_idle=True)
+        time.sleep(4)
+        self.gui.clear()
+        self.enclosure.display_manager.remove_active()
+        self.play_sky_live_stream({})
+    
+    @intent_file_handler("play_sky_live_stream.intent")
+    def play_sky_live_stream(self, message):
+        # Get the URL of where the live stream is playing
+        set_url = "https://skynews2-plutolive-vo.akamaized.net/cdhlsskynewsamericas/1013/latest.m3u8?serverSideAds=true"
+        # Sending the stream to my GUI, creating a sessionData object
+        self.gui["videoState"] = "Play"
+        self.gui["videoStream"] = set_url
+        # This is the session data that is being sent to the qml page
+        # Asking the skill to display a page that will contain my video player
+        # Let's create the qml user interface for this
+        self.gui.show_page("SkyStream.qml", override_idle=True)
+    
+    @intent_file_handler("stop_sky_live_stream.intent")
+    def stop_sky_live_stream(self, message):
+        self.gui["videoState"] = "Stop"
+        
+def create_skill():
+    return SkyStream()
